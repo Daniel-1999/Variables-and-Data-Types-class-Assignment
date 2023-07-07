edcc16f3a87eea02f2231181e03cfb16884b95e7
Daniel-1999 <bakuoruluki@gmail.com>
2023-07-07 11:58:49 +0000

    solutions to my first assignment

---
+++ Luki.js
@@ -0,0 +1,15 @@
+// summation of numbers
+const numOne = 10;
+const numTwo = 24;
+const numThree = 21;
+const result = numOne + numTwo + numThree;
+console.log(`the sum of the three numbers is ${result}`);
+
+const fullName = "Luki Daniel Bakuoru";
+const age = Number(23);
+const isStudent = false;
+let hobbies = ["reading novels", "watching football", "watching documentaries"];
+console.log(fullName);
+console.log(age);
+console.log(isStudent);
+console.log(hobbies);
